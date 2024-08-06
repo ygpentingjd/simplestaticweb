32cd02ecaa3519cb0deba1c8519e929ade04af95
yangpentingjadi <cc.banyumas@gmail.com>
2024-08-06 14:19:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 // Mendapatkan alamat IP server
 $server_ip = $_SERVER['SERVER_ADDR'];
-
+$kalimat = "HALLOOOO INI PERCOBAAN SAJA -CHRIS";
 // Menampilkan alamat IP server
 echo "Alamat IP server adalah: " . $server_ip;
+
+echo $kalimat;
 ?>
